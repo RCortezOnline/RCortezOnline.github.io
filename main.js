be7c60682f3d7e00ace1ee296ba73c78288d59d4
RCortezOnline <RCortez.online@gmail.com>
2021-08-12 08:49:40 -0400

    first push

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+let rightClickedItem = null;
+
+if (document.body) {
+  document.body.addEventListener((getElementById("about"), (e) => {
+    rightClickedItem = e
+  });
+  document.body.addEventListener('click', () => {
+    rightClickedItem = null;
+  });
+}
+if (document.body) {
+  document.body.addEventListener((getElementById("projects"), (e) => {
+    rightClickedItem = e
+  });
+  document.body.addEventListener('click', () => {
+    rightClickedItem = null;
+  });
+}
+if (document.body) {
+  document.body.addEventListener((getElementById("contact"), (e) => {
+    rightClickedItem = e
+  });
+  document.body.addEventListener('click', () => {
+    rightClickedItem = null;
+  });
+}
